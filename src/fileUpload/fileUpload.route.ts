e8799a9bd82c581d15c667cda2395b69f89e082a
ABU TAHER <abutaher267@gmail.com>
2024-10-18 21:09:50 +0600

    remove file upload

--- src/fileUpload/fileUpload.route.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import controllers from './fileUpload.controller';
-import validations from './fileUpload.validation';
-const router = express.Router();
-router.post('/', validations.isValidUploadFile, controllers.imageUpload);
-router.get('/', controllers.allImages);
-router.delete('/:id', controllers.imageDelete);
-export default router;
